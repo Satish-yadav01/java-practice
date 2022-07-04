b0d915d9e59a50cab58edd2089fdc7d3c0ebe6ad
satish yadav <yadavabhishekay05@gmail.com>
2022-07-04 10:19:05 +0530

    OneSideNegativeElement added without logic

---
+++ src/array/OneSideNegativeElement.java
@@ -0,0 +1,12 @@
+//Move all the negative elements to one side of the array
+
+package array;
+
+import java.util.Arrays;
+
+public class OneSideNegativeElement {
+    public static void main(String[] args) {
+        int a[] = {1,2,-4,-5,0,-2,-8,6,8};
+
+    }
+}
